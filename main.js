ec54356bddcf30cbccef1f2c71016072b6a9fa5e
Eduardo Candido <eduardo_sbk589@hotmail.com>
2021-09-02 14:55:12 -0400

    architect practice project

---
+++ main.js
@@ -0,0 +1,19 @@
+
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector('body').classList.add('display')
+  }, 3000);
+};
+
+
+
+document.querySelector('.hamburguer-menu').addEventListener('click', () => {
+  document.querySelector('.container').classList.toggle('change');
+  document.querySelector('body').classList.toggle('active');
+});
+
+function closeMenu() {
+  document.querySelector('.container').classList.toggle('change');
+  document.querySelector('body').classList.remove('active');
+
+}
